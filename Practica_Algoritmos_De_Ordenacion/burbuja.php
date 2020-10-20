eefa29b01fe92e53c4a613d6bdd7fb083b67bc95
narenaruiz <narena@cifpfbmoll.eu>
2020-10-20 21:04:21 +0200

    Estoy probando e intentando ver como lo puedo hacer

---
+++ Practica_Algoritmos_De_Ordenacion/burbuja.php
@@ -0,0 +1,13 @@
+<?php
+//Algoritmo de ordenación: Burbuja
+$lista = array(4, 7, 5, 10, 6, 2, 8, 3, 1, 9);
+$listaOrdenada = false;
+$posicionComprobar = 0;
+$numeroMayor = $lista[$posicionComprobar];
+
+while (!$listaOrdenada) {
+    if ($lista[$posicionComprobar] > $lista[$posicionComprobar + 1]) {
+        
+    }
+}
+?>
