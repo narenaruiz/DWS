efaa8dc25cce9cfbd8538d254ce2420fb37260c0
narenaruiz <narena@cifpfbmoll.eu>
2020-10-20 20:32:07 +0200

    Creo que esta casi listo

--- Practica_Algoritmos_De_Ordenacion/BusquedaBinaria.php
+++ Practica_Algoritmos_De_Ordenacion/BusquedaBinaria.php
@@ -1,22 +1,26 @@
 <?php
 //Busqueda Binaria
 $lista = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-//HACER UN WHILE Y DEPUES DOS IF PARA COMPROBAR SI EL NUMERO ES EL DE EN MEDIO Y OTRO PARA VER QUE LADO NOS QUEDAMOS. 
-$repeticion = false;
 $numeroBuscado = 3;
+$numeroEncontrado = false;
 $indiceAlto = count($lista);
 $indiceBajo = 0;
 $posicionNumero = 0;
-while (!$repeticion) {
+while (!$numeroEncontrado) {
     $centro = ($indiceBajo + $indiceAlto)/2;
     if ($centro > $numeroBuscado) {
         $indiceAlto = $centro;
-        $indiceBajo = 0;
+        $indiceBajo = $indiceBajo;
     } else if ($centro < $numeroBuscado) {
-        $indiceAlto = ;
+        $indiceAlto = $indiceAlto;
         $indiceBajo = $centro;
+    } else if ($centro == $numeroBuscado) {
+        $posicionNumero = $centro;
+        echo "El numero a sido encontrado es " . $numeroBuscado . " y esta en la posicion " . $posicionNumero . " de la lista.";
+        $numeroEncontrado = true;
     } else {
-        echo "El numero a sido encontrado es " . $numeroBuscado . " y esta en la posicion " . $posicionNumero . " de la lista."
+        echo "El numero " . $numeroBuscado . " no esta en la lista";
+        $numeroEncontrado = true;
     }
 }
 ?>
