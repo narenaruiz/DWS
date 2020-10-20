3e2700b9cb6a06cbb3d082d68483409d8d569097
narenaruiz <narena@cifpfbmoll.eu>
2020-10-20 19:49:19 +0200

    Haciendo la busqueda binaria

--- Practica_Algoritmos_De_Ordenacion/BusquedaBinaria.php
+++ Practica_Algoritmos_De_Ordenacion/BusquedaBinaria.php
@@ -1,12 +1,22 @@
 <?php
 //Busqueda Binaria
 $lista = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-echo $lista[4];
 //HACER UN WHILE Y DEPUES DOS IF PARA COMPROBAR SI EL NUMERO ES EL DE EN MEDIO Y OTRO PARA VER QUE LADO NOS QUEDAMOS. 
 $repeticion = false;
-
+$numeroBuscado = 3;
+$indiceAlto = count($lista);
+$indiceBajo = 0;
+$posicionNumero = 0;
 while (!$repeticion) {
-    $centro = (0 + count($lista))/2;
-    echo $lista[$centro];
+    $centro = ($indiceBajo + $indiceAlto)/2;
+    if ($centro > $numeroBuscado) {
+        $indiceAlto = $centro;
+        $indiceBajo = 0;
+    } else if ($centro < $numeroBuscado) {
+        $indiceAlto = ;
+        $indiceBajo = $centro;
+    } else {
+        echo "El numero a sido encontrado es " . $numeroBuscado . " y esta en la posicion " . $posicionNumero . " de la lista."
+    }
 }
 ?>
