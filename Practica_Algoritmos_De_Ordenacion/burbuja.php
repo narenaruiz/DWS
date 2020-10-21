e1038e3f3f773cd731cb1a25a1ec2db6acd6bf82
narenaruiz <narena@cifpfbmoll.eu>
2020-10-21 17:32:01 +0200

    He acabado la funcion del algoritmo de ordenacion burbuja

--- Practica_Algoritmos_De_Ordenacion/burbuja.php
+++ Practica_Algoritmos_De_Ordenacion/burbuja.php
@@ -1,17 +1,34 @@
 <?php
+function burbuja ($lista){
 //Algoritmo de ordenación: Burbuja
-$lista = array(4, 7, 5, 10, 6, 2, 8, 3, 1, 9);
-$listaOrdenada = false;
-$lengthLista = count($lista);
-$posicionComprobar = 0;
-$numeroCambio = 0;
 
-for ($x = 0; $x <= $lengthLista; $x++) {
-    $posicionComprobar = $x;
-    if ($lista[$posicionComprobar] > $lista[$posicionComprobar + 1]) {
-        $numeroCambio = $lista[$posicionComprobar];
-        $lista[$posicionComprobar] = $lista[$posicionComprobar + 1];
-        $lista[$posicionComprobar + 1] = $numeroCambio;
+//La dejo vacia para que se rellene con lo que reciba la funcion.
+$lista = array();
+$lengthLista = count($lista);
+$numeroCambio = 0;
+/**
+ * Si solo pongo el menor la x llegara hasta la
+ * posicion 9 y si pongo menor o igual me llegara
+ * hasta la posicion 11, por lo tanto tengo que
+ * restarle uno para que llegue hasta la posicion 8
+ * y por lo tanto pueda sumar 1 para hacer la
+ * comprobacion. Y utilizo la 'y' para la resta
+ * para que cuando vuelva a pasar llegue hasta
+ * una posicion menos ya que el ultimo numero
+ * ya estara colocado como el mas grande.
+ */
+for ($y = 1; $y < $lengthLista; $y++) {
+    for ($x = 0; $x < $lengthLista - $y; $x++) {
+        if ($lista[$x] > $lista[$x + 1]) {
+            $numeroCambio = $lista[$x];
+            $lista[$x] = $lista[$x + 1];
+            $lista[$x + 1] = $numeroCambio;
+        }
     }
+}
+/*
+Esto es para imprimir la lista de ser necesaria una prueba.
+print_r($lista);
+*/
 }
 ?>
