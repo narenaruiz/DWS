4e7a306b2909233b3dbaefb615c5862a26e9ab56
narenaruiz <narena@cifpfbmoll.eu>
2020-10-26 19:56:43 +0100

    He hecho el tercer algoritmo y he modificado las opciones

--- Practica_Algoritmos_De_Ordenacion/practica.php
+++ Practica_Algoritmos_De_Ordenacion/practica.php
@@ -1,12 +1,13 @@
 <?php
-
-//Si hemos enviado el formulario entonces se ejecutara lo del if
+/**
+ * Si hemos enviado el formulario entonces se ejecutara
+ * lo del if si no entonces simplemente saldra el html
+ */
 if(isset($_POST['envioFormulario'])) {
 
     //Recoger los dos valores de los radio buttons
     $opcionAlgoritmo = $_POST['opcionAlgoritmo'];
     $tipoArray = $_POST['tipoArray'];
-
 
     /**
      * Esta funcion es la que ejecuta la busqueda
@@ -18,7 +19,7 @@ if(isset($_POST['envioFormulario'])) {
      * que hay que buscar y sera
      * proporcionado por el usuario
      */
-    function busquedaBinaria($lista, $numeroBuscado){
+    function busquedaBinaria($lista, $numeroBuscado) {
         $indiceAlto = count($lista);
         $indiceBajo = 0;
         
@@ -52,7 +53,7 @@ if(isset($_POST['envioFormulario'])) {
      * 
      * @param lista es la array que recibe para ordenar
      */
-    function burbuja ($lista){
+    function burbuja($lista) {
         $lengthLista = count($lista);
         $numeroCambio = 0;
         for ($y = 1; $y < $lengthLista; $y++) {
@@ -76,8 +77,46 @@ if(isset($_POST['envioFormulario'])) {
     }
 
     /**
+     * Esta funcion es la que ejecuta el algoritmo de
+     * ordenacion intercambio.
+     * 
+     * @param lista es la array que recibe para ordenar
+     */
+    function intercambio($lista) {
+        $lengthLista = count($lista);
+        /**
+         * Numero que se compara con todos los demas hasta encontrar uno menor
+         * y entonces se intercambiany el nuevo numero se seguira comparando con
+         * los numeros que queden
+         */
+        $numComparar = 0;
+        //Numero usado para guardar temporalmente un numero
+        $numSustituir = 0;
+        
+        while ($numComparar < $lengthLista) {
+            for ($y = ($numComparar + 1); $y < $lengthLista; $y++) {
+                if ($lista[$numComparar] > $lista[$y]) {
+                    $numSustituir = $lista[$numComparar];
+                    $lista[$numComparar] = $lista[$y];
+                    $lista[$y] = $numSustituir;
+                }
+            }
+            $numComparar++;
+        }
+        
+        $result = "La array ordenada queda asi:";
+        foreach ($lista as $num) {
+            $result .= " " . $num;
+        }
+        return $result;
+    }
+
+    /**
      * Funcion para la opcion de busqueda binaria con
      * un array de numeros predeterminado
+     * 
+     * @param numeroBuscado es el numero que el usuario
+     * quiere buscar
     */
     function opcionListaPredeterminadaBinario($numeroBuscado) {
         $lista = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
@@ -96,17 +135,13 @@ if(isset($_POST['envioFormulario'])) {
         return $resultado;
     }
 
-    $resultadoFinal = "";
-switch($tipoArray){
-    case "arrayPredBinario":
-        //Guardamos el valor enviado en el formulario
-        $numeroBuscado = $_POST['numSearch'];
-        $resultadoFinal = opcionListaPredeterminadaBinario($numeroBuscado);
-        break;
-    case "arrayPredBurbuja":
-        $resultadoFinal = opcionListaPredeterminadaBurbuja();
-        break;
-    case "arrayRandomBurbuja":
+    function opcionListaPredeterminadaIntercambio() {
+        $lista = array(6, 4, 7, 2, 9, 3, 10, 5, 1, 8);
+        $resultado = intercambio($lista);
+        return $resultado;
+    }
+
+    function crearListaRandom() {
         //Guardamos en una variable la cantidad de numero aleatorios solicitados, lo recogemos mediante $_POST
         $numerosRandom = $_POST['numerosRandom'];
         $lista = array();
@@ -115,8 +150,39 @@ switch($tipoArray){
         for($x = 0; $x < $numerosRandom; $x++){
             array_push($lista,rand(1, 100));
         }
+        return $lista;
+    }
 
-        $resultadoFinal = burbuja($lista);
+    $resultadoFinal = "";
+    /**
+     * El switch recibe el id de los que tienen el name tipoArray el
+     * cual se ha guardado en la variable con el mismo nombre
+     * 
+     * @param tipoArray es la variable que tiene guardado el id que
+     * dentro del switch se comparara con los distintos case
+     */
+switch($tipoArray){
+    case "arrayPredBinario":
+        //Guardamos el valor enviado en el formulario
+        $numeroBuscado = $_POST['numSearch'];
+        //Opcion de la busqueda binaria con una lista predeterminada ordenada
+        $resultadoFinal = opcionListaPredeterminadaBinario($numeroBuscado);
+        break;
+    case "arrayPredBurbuja":
+        //Opcion de burbuja con una lista predeterminada desordenada
+        $resultadoFinal = opcionListaPredeterminadaBurbuja();
+        break;
+    case "arrayPredIntercambio":
+        //Opcion de intercambio con una lista predeterminada desordenada
+        $resultadoFinal = opcionListaPredeterminadaIntercambio();
+        break;
+    case "arrayRandomBurbuja":
+        //Opcion de burbuja pero con una funcion que te dara una lista desordenada
+        $resultadoFinal = burbuja(crearListaRandom());
+        break;
+    case "arrayRandomIntercambio":
+        //Opcion de intercambio pero con una funcion que te dara una lista desordenada
+        $resultadoFinal = intercambio(crearListaRandom());
         break;
     case "arrayUsuario":
         /**
@@ -155,11 +221,11 @@ switch($tipoArray){
     <title>Practica Algoritmos</title>
 </head>
 <body>
-    <!-- Hacer un formulario para enviar la
-    informacion ha la misma pagina y entonces
-    que el php te la recoja y ejecute la funcion -->
-
     <form action="practica.php" method="post">
+    <!-- 
+        Creo que esto sobra y que las opciones de abajo ya sirve
+        para lo que quiera hacer el usuario:
+
         <label>Elegir el tipo de Algoritmo:</label>
         <br>
         <input type="radio" name="opcionAlgoritmo" id="algoritmoBinario" value="Binario" checked>
@@ -169,13 +235,19 @@ switch($tipoArray){
         <label for="algoritmoBurbuja">Burbuja</label>
         <br>
         <br>
-        <label>Opcion tipo lista:</label>
+
+        -->
+        <label>Opcion de algoritmo y tipo de lista:</label>
+        <br>
         <input type="radio" name="tipoArray" id="arrayPredBinario" value="arrayPredBinario" checked>
-		<label for="arrayPredBinario">Matriz predeterminada para binario: 1, 2, ..., 9, 10 / Di que numero quieres buscar:<label>
+		<label for="arrayPredBinario">Matriz predeterminada para binario: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 - Di que numero quieres buscar:<label>
         <input type="number" name="numSearch" value = "2">
         <br>
-        <input type="radio" name="tipoArray" id="arrayPredBurbuja" value="arrayPredBurbuja" checked>
+        <input type="radio" name="tipoArray" id="arrayPredBurbuja" value="arrayPredBurbuja">
 		<label for="arrayPredBurbuja">Matriz predeterminada para burbuja: 4, 7, 5, 10, 6, 2, 8, 3, 1, 9<label>
+        <br>
+        <input type="radio" name="tipoArray" id="arrayPredIntercambio" value="arrayPredIntercambio">
+		<label for="arrayPredIntercambio">Matriz predeterminada para intercambio: 6, 4, 7, 2, 9, 3, 10, 5, 1, 8<label>
         <br>
 		<input type="radio" name="tipoArray" id="arrayRandomBurbuja" value="arrayRandomBurbuja">
 		<label for="arrayRandomBurbuja">Di la longitud del array aleatorio que quieres crear para la burbuja:</label>
