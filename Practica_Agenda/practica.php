72268aba4149c06e7f007547bcc2e2931e282377
narenaruiz <narena@cifpfbmoll.eu>
2020-10-28 21:17:31 +0100

    Creo que he acabado

--- Practica_Agenda/practica.php
+++ Practica_Agenda/practica.php
@@ -22,10 +22,12 @@
         <?php
             /**
              * @author Nicolás Arena Ruiz <narena@cifpfbmoll.eu>
-             * @version 1.0.4
+             * @version 1.0.5
              */
 
-            /**
+            /*
+             * Te  guarda los datos
+             * 
              * Cuando el input hidden este vacio (cuando cargas la pagina y no has
              * enviado nada todavia) entonces se te ejecutara este if en el cual te
              * inicializa la lista ($lista) donde guardaras todos los datos.
@@ -34,12 +36,8 @@
              * te guardara en $lista y luego crea un nuevo array ($newValue),
              * despues habra algunas condiciones para ejecutar cierto codigo
              * 
-             * @var lista variable del primer if, contiene un metodo el cual hace que sea un array
-             * @var newValue variable del else, contiene un metodo el cual hace que sea un array
-             * @method array() metodo en la primera y segunda variable, te contruye el array
-             * @method parse_str() metodo usado en el else, transforma un string en un array, en
-             * este caso es lo que hay guardado en el input hidden lo que vuelve un array y lo guarda
-             * en la array $lista
+             * @var lista contiene un metodo el cual hace que sea un array
+             * @var newValue contiene un metodo el cual hace que sea un array
              * @link parse_str: https://www.php.net/manual/es/function.parse-str.php
              */
             if (!isset($_POST['guardar'])) {
@@ -49,14 +47,14 @@
                 //El parse_str te vuelve el string que esta en guardar en un array que te guarda en lista
                 parse_str($_POST['guardar'], $lista);
                 $newValue = array();
-                /**
+                /*
                  * En caso de que el nombre y el telefono hayan sido escritos guardara
                  * estos dos datos en la array construida anteriormente ($newValue)
                  */
                 if(!empty($_POST['nameUser']) && !empty($_POST['telUser'])) {
                     $newValue = array($_POST['nameUser'] => $_POST['telUser']);
                 }
-                /**
+                /*
                  * Cuando el telefono esta vacio y el nombre no esta vacio entonces te borra
                  * de la lista la clave que tenga el mismo nombre y te borra su
                  * valor tambien.
@@ -65,13 +63,11 @@
                 if (empty($_POST['telUser']) && !empty($_POST['nameUser'])) {
                     unset($lista[$_POST['nameUser']]);
                 }
-                /**
-                 * sobrescribo la variable guardando lo suyo mas lo otro
-                 */
+                //Sobrescribo la variable guardando lo suyo mas lo otro
                 $lista = array_merge($lista, $newValue);
             }
 
-            /**
+            /*
              * Te transforma el array ($lista) en un url
              * @link http_build_query: https://www.php.net/manual/es/function.http-build-query.php
              */
@@ -79,23 +75,21 @@
                 $listatxt = http_build_query($lista);
             }
 
-            /**
+            /*
              * Creo un input hidden el cual es un input pero que no se ve
              * y en un inicio esta vacio
              */
             echo '<input type="hidden" name="guardar" id="guardar" value="'.$listatxt.'">';
                 
             if (isset($_POST['envioFormulario'])) {
-                /**
+                /*
                  * Esta condicion hace que salga un mensaje en caso de que
                  * no introduzcas el nombre
                  */
                 if (empty($_POST['nameUser'])) {
                     echo "<h3>El nombre no ha sido introducido</h3>";
                 }
-                /**
-                 * Te imprime los datos
-                 */
+                //Te imprime los datos
                 echo "<h2>Datos Guardados</h2>";
                 echo "<ul>";
                 foreach($lista as $name => $tel) {
