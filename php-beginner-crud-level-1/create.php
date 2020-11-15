336a26730301978d7b049c1f2553ff93d206d269
narenaruiz <narena@cifpfbmoll.eu>
2020-11-15 20:04:42 +0100

    He arreglado el problema anterior pero ahora me falla algo en alguna parte del 6

--- php-beginner-crud-level-1/create.php
+++ php-beginner-crud-level-1/create.php
@@ -18,49 +18,49 @@
         <div class="container">
 
             <div class="page-header">
-                <<h1>Create Products</h1>
+                <h1>Create Products</h1>
             </div>
 
             <!-- apartado 5.3 -->
             <?php
-            if($_POST){
-                
+            if ($_POST) {
+
                 // include database connection
                 include 'config/database.php';
-                
+
                 try {
-                    
+
                     // insert query
                     $query = "INSERT INTO products SET name=:name, description=:description, price=:price, created=:created";
-                    
+
                     // prepare query for execution
                     $stmt = $con->prepare($query);
-                    
+
                     // posted values
                     //Enlace explicativo de la funcion strip_tags: https://www.php.net/manual/es/function.strip-tags.php
-                    $name=htmlspecialchars(strip_tags($_POST['name']));
-                    $description=htmlspecialchars(strip_tags($_POST['description']));
-                    $price=htmlspecialchars(strip_tags($_POST['price']));
-                    
+                    $name = htmlspecialchars(strip_tags($_POST['name']));
+                    $description = htmlspecialchars(strip_tags($_POST['description']));
+                    $price = htmlspecialchars(strip_tags($_POST['price']));
+
                     // bind the paremeters
                     $stmt->bindParam(':name', $name);
                     $stmt->bindParam(':description', $description);
                     $stmt->bindParam(':price', $price);
-                    
+
                     // specify when this record was inserted to the database
-                    $created=date('Y-m-d H:i:s');
+                    $created = date('Y-m-d H:i:s');
                     $stmt->bindParam(':created', $created);
-                    
+
                     // Execute the query
-                    if($stmt->execute()){
+                    if ($stmt->execute()) {
                         echo "<div class='alert alert-success'>Record was saved.</div>";
                     } else {
                         echo "<div class='alert alert-danger'>Unable to save record.</div>";
                     }
                 }
-                
+
                 // show error
-                catch(PDOException $exception) {
+                catch (PDOException $exception) {
                     die('Error: ' . $exception->getMessage());
                 }
             }
