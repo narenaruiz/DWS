19a88662e2c6f57abe2324edddfa0715973bac57
narenaruiz <narena@cifpfbmoll.eu>
2020-11-16 12:16:41 +0100

    El error estaba en el 6.4 pero no he encontrado ninguna diferencia del mio con el de la pagina asi que he copiado ese apartado con copiar y pegar y ha funcionado y no he entendido porque. Ademas antes no me saltaba ningun error simplemente no se veian las filas de la base de datos

--- php-beginner-crud-level-1/index.php
+++ php-beginner-crud-level-1/index.php
@@ -61,29 +61,29 @@
                 // retrieve our table contents
                 // fetch() is faster than fetchAll()
                 // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    // extract row
-                    // this will make $row['firstname'] to
-                    //just $firstname only
-                    extract($row);
-                    
-                    // creating new table row per record
-                    echo "<tr>";
-                        echo "<td>{$id}</td>";
-                        echo "<td>{$name}</td>";
-                        echo "<td>{$description}</td>";
-                        echo "<td>{$price}</td>";
-                        echo "<td>";
-                            // read one record
-                            echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
-                            
-                            //we will use this links on next part of this post
-                            echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
- 
-                            // we will use this links on next part of this post
-                            echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
-                        echo "</td>";
-                    echo "</tr>";
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                        // extract row
+                        // this will make $row['firstname'] to
+                        // just $firstname only
+                        extract($row);
+	
+                        // creating new table row per record
+                        echo "<tr>";
+                                echo "<td>{$id}</td>";
+                                echo "<td>{$name}</td>";
+                                echo "<td>{$description}</td>";
+                                echo "<td>{$price}</td>";
+                                echo "<td>";
+                                        // read one record 
+                                        echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+			
+                                        // we will use this links on next part of this post
+                                        echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+                                        // we will use this links on next part of this post
+                                        echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+                                echo "</td>";
+                        echo "</tr>";
                 }
                 
             // end table
