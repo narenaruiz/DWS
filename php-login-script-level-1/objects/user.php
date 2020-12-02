fca9473b51568130456d7f4fd06163f0955be741
narenaruiz <narena@cifpfbmoll.eu>
2020-12-02 19:30:35 +0100

    He acabado el apartado 10

--- php-login-script-level-1/objects/user.php
+++ php-login-script-level-1/objects/user.php
@@ -140,4 +140,57 @@ class User {
     }
 
     //------ Fin apartado 8.7 ---------
+    // Apartado 10.2
+    // read all user records
+    function readAll($from_record_num, $records_per_page) {
+
+        // query to read all user records, with limit clause for pagination
+        $query = "SELECT
+				id,
+				firstname,
+				lastname,
+				email,
+				contact_number,
+				access_level,
+				created
+			FROM " . $this->table_name . "
+			ORDER BY id DESC
+			LIMIT ?, ?";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // bind limit clause variables
+        $stmt->bindParam(1, $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(2, $records_per_page, PDO::PARAM_INT);
+
+        // execute query
+        $stmt->execute();
+
+        // return values
+        return $stmt;
+    }
+
+    //----- Fin apartado 10.2 -----
+    // Apartado 10.4
+    // used for paging users
+    public function countAll() {
+
+        // query to select all user records
+        $query = "SELECT id FROM " . $this->table_name . "";
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        // get number of rows
+        $num = $stmt->rowCount();
+
+        // return row count
+        return $num;
+    }
+
+    //----- Fin apartado 10.4 -----
 }
