7ef8e6db36e1c6176b9ace7111dc170cedec3d04
narenaruiz <narena@cifpfbmoll.eu>
2020-12-02 20:11:18 +0100

    He acabado el apartado 12

--- php-login-script-level-1/login.php
+++ php-login-script-level-1/login.php
@@ -121,6 +121,11 @@ echo "<div class='col-sm-6 col-md-4 col-md-offset-4'>";
 					echo "<input type='text' name='email' class='form-control' placeholder='Email' required autofocus />";
 					echo "<input type='password' name='password' class='form-control' placeholder='Password' required />";
 					echo "<input type='submit' class='btn btn-lg btn-primary btn-block' value='Log In' />";
+                                        // Apartado 12.1
+                                        echo "<div class='margin-1em-zero text-align-center'>
+                                        <a href='{$home_url}forgot_password'>Forgot password?</a>
+                                        </div>";
+                                        // -----Fin apartado 12.1 ----
 				echo "</form>";
 			echo "</div>";
 		echo "</div>";
