f93f82f2f5d6fc00dd01caf1cc15eb0be695d5ba
narenaruiz <narena@cifpfbmoll.eu>
2020-12-02 17:01:53 +0100

    He acabado el apartado 6 de login

--- php-login-script-level-1/layout_foot.php
+++ php-login-script-level-1/layout_foot.php
@@ -1,8 +1,13 @@
-<?php
+	</div>
+	<!-- /container -->
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!-- jQuery library -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 
+<!-- Bootstrap JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- end HTML page -->
+</body>
+</html>
+<!-- Lo anterior es el apartado 6.5 -->
