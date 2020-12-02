90a6e944f1ffd64a7864d097acddb2a93e8e248e
narenaruiz <narena@cifpfbmoll.eu>
2020-12-02 17:49:26 +0100

    He acabado el apartado 8, en la ultima parte de este apartado hay un trozo de codigo que no se especifica donde va pero he supuesto que seria en user.php

--- php-login-script-level-1/libs/php/utils.php
+++ php-login-script-level-1/libs/php/utils.php
@@ -1,8 +1,6 @@
+<!-- Apartado 8.2 -->
 <?php
+class Utils{
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+}
+?>
