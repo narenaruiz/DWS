24380a434c1d35f2cba88cc9806a67b5a8b3db6d
narenaruiz <narena@cifpfbmoll.eu>
2020-12-08 12:42:16 +0100

    Apartado 7 acabado, voy por el 8

--- php_oop_crud_level_1/objects/product.php
+++ php_oop_crud_level_1/objects/product.php
@@ -71,15 +71,76 @@ function readAll($from_record_num, $records_per_page) {
 }
 
 // used for paging products
-public function countAll(){
- 
+//comento el public porque sino sale error
+/* public */ function countAll() {
+
     $query = "SELECT id FROM " . $this->table_name . "";
- 
-    $stmt = $this->conn->prepare( $query );
+
+    $stmt = $this->conn->prepare($query);
     $stmt->execute();
- 
+
     $num = $stmt->rowCount();
- 
+
     return $num;
 }
+
+function readOne() {
+
+    $query = "SELECT
+                name, price, description, category_id
+            FROM
+                " . $this->table_name . "
+            WHERE
+                id = ?
+            LIMIT
+                0,1";
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $this->id);
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $this->name = $row['name'];
+    $this->price = $row['price'];
+    $this->description = $row['description'];
+    $this->category_id = $row['category_id'];
+}
+
+function update() {
+
+    $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                name = :name,
+                price = :price,
+                description = :description,
+                category_id  = :category_id
+            WHERE
+                id = :id";
+
+    $stmt = $this->conn->prepare($query);
+
+    // posted values
+    $this->name = htmlspecialchars(strip_tags($this->name));
+    $this->price = htmlspecialchars(strip_tags($this->price));
+    $this->description = htmlspecialchars(strip_tags($this->description));
+    $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    // bind parameters
+    $stmt->bindParam(':name', $this->name);
+    $stmt->bindParam(':price', $this->price);
+    $stmt->bindParam(':description', $this->description);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':id', $this->id);
+
+    // execute the query
+    if ($stmt->execute()) {
+        return true;
+    }
+
+    return false;
+}
+
 ?>
