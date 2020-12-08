d3d17aa03a8cedf48df24e82961cdadd971433ea
narenaruiz <narena@cifpfbmoll.eu>
2020-12-08 15:47:57 +0100

    He acabado el tutorial

--- php_oop_crud_level_1/create_product.php
+++ php_oop_crud_level_1/create_product.php
@@ -29,10 +29,16 @@ if($_POST){
     $product->price = $_POST['price'];
     $product->description = $_POST['description'];
     $product->category_id = $_POST['category_id'];
+    $image=!empty($_FILES["image"]["name"])
+        ? sha1_file($_FILES['image']['tmp_name']) . "-" . basename($_FILES["image"]["name"]) : "";
+    $product->image = $image;
  
     // create the product
     if($product->create()){
         echo "<div class='alert alert-success'>Product was created.</div>";
+        // try to upload the submitted file
+        // uploadPhoto() method will return an error message, if any.
+        echo $product->uploadPhoto();
     }
  
     // if unable to create the product, tell the user
@@ -43,7 +49,7 @@ if($_POST){
 ?>
  
 <!-- HTML form for creating a product -->
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
  
     <table class='table table-hover table-responsive table-bordered'>
  
@@ -81,6 +87,10 @@ echo "<select class='form-control' name='category_id'>";
 echo "</select>";
 ?>
             </td>
+        </tr>
+        <tr>
+            <td>Photo</td>
+            <td><input type="file" name="image" /></td>
         </tr>
  
         <tr>
